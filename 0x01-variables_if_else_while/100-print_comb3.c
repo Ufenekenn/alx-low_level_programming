76678e6388f428c65a747bbbc4270f8e75ba8d34
ufenekenn <ufenekenn@gmail.com>
2022-10-16 05:44:33 -0700

    execute

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,13 +11,13 @@ int main(void)
 	i = 48;
 	e = 48;
 
-	while (e < 48)
+	while (e < 58)
 	{
 		i = 48;
 		while (i < 58)
 		{
-			putchar(i);
 			putchar(e);
+			putchar(i);
 			if (i == 57 && e == 57)
 			{
 				break;
