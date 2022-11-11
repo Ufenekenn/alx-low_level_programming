9bf2befc9ffbf524c355891b40bfa2b2cd199cf9
ufenekenn <ufenekenn@gmail.com>
2022-11-11 04:37:40 -0800

    First commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -4,7 +4,7 @@
 /**
  * string_nconcat - concatenates two strings.
  * @s1: first string.
- * @s2: second string.
+ * @n: amount of bytes.
  *
  * Return: pointer to the allocated memory.
  * if malloc fails, status value is equal to 98.
